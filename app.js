0db8c3df7d2e94f8a898574d11bbe27e09134bde
Marcomussa <marcomussa567@gmail.com>
2021-08-27 14:00:49 -0300

    Se agrego funcionalidad de numeros par e inpar

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+let arr = [45,2,94,41,35,21,25,66,47,353,6,89,88,34]
+
+//! Mostrar Numeros Pares
+function mostrarPares(par){
+    let pares = []
+    let inpares = []
+    for(let i = 0; i < par.length; i++){
+        if(arr[i] % 2 === 0){
+            pares.push(par[i])
+        }
+        else {
+            inpares.push(par[i])
+        }
+    }
+    return {
+        "Pares": pares,
+        "Inpares": inpares,
+        "Cantidad de Numeros": par.length,
+        "Cantidad de Pares": pares.length,
+        "Cantidad de Inpares": inpares.length
+    }
+}
+
+console.log(mostrarPares(arr))
