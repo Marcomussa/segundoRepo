3a6569618aaaede4bf06012668d578ee487d85c1
Marcomussa <marcomussa567@gmail.com>
2021-08-29 17:19:09 -0300

    Se agrego funcionalidad sobre mostrar numeros mayores y menores a diez

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let arr = [45,2,94,41,35,21,25,66,47,353,6,89,88,34]
 
-//! Mostrar Numeros Pares
+//! Mostrar Numeros Pares e Impares
 function mostrarPares(par){
     let pares = []
     let inpares = []
@@ -21,4 +21,22 @@ function mostrarPares(par){
     }
 }
 
-console.log(mostrarPares(arr))
+//! Mostrar Numeros Mayores/Menores a 10
+function filtrar(par){
+    let mayoresADiez = []
+    let menoresADiez = []
+    let filtrarMayorADiez = par.filter( (par) => {
+        return par >= 10
+    })
+    let filtrarMenorADiez = par.filter( (par) => {
+        return par <= 10
+    })
+    mayoresADiez.push(...filtrarMayorADiez)
+    menoresADiez.push(...filtrarMenorADiez)
+    return {
+        'Menores a 10:': menoresADiez,
+        'Mayores a 10:': mayoresADiez
+    }
+}
+
+console.log(filtrar(arr))
